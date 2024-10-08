bd0d1cfc4b1c00f075015bc6575ca33832a47648
BRENNO22 <brennorodriguesdeoliveirarodri@gmail.com>
2024-10-08 10:58:03 -0300

    BRENNOATT3.c

---
+++ BREENO_ATT3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+
+int idadebrenno = 23;
+int idadejoel = 23;
+
+if(idadebrenno > idadejoel){
+
+    printf ("brenno é mais velho que joel");
+}
+    else printf ("joel é mais velho que brenno");
+    
+    if (idadebrenno == idadejoel){ 
+        printf ("ninguem liga");
+    }
+   }
